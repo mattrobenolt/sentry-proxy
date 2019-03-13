8dcd35e057430af820b5574045ba4d5de321aacf
Matt Robenolt <matt@ydekproductions.com>
2019-03-12 18:47:21 -0700

    Logging and other enhancements

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module sentry-proxy
 
-require github.com/buger/jsonparser v0.0.0-20181115193947-bf1c66bbce23
+require (
+	github.com/buger/jsonparser v0.0.0-20181115193947-bf1c66bbce23
+	github.com/go-http-utils/logger v0.0.0-20161128092850-f3a42dcdeae6
+	github.com/stretchr/testify v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"bytes"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 	"regexp"
+	"runtime"
 	"time"
 
 	"github.com/buger/jsonparser"
+	"github.com/go-http-utils/logger"
 )
 
-var upstreamFlag = flag.String("upstream", "https://sentry.io", "Upstream Sentry server")
+const Version = "0.0.0"
+const MaxRequestLength = 250 * 1024
+
+var (
+	upstreamFlag     = flag.String("upstream", "https://sentry.io", "Upstream Sentry server")
+	listenFlag       = flag.String("listen", "127.0.0.1:8080", "Address to bind to")
+	readTimeoutFlag  = flag.Duration("read-timeout", 10*time.Second, "Read timeout")
+	writeTimeoutFlag = flag.Duration("write-timeout", 10*time.Second, "Write timeout")
+	versionFlag      = flag.Bool("version", false, "Print program version")
+)
 
 func newHandler(upstream string) http.HandlerFunc {
 	u, _ := url.Parse(upstream)
@@ -36,41 +49,72 @@ func newHandler(upstream string) http.HandlerFunc {
 		// Make sure we have a valid client IP address to work with
 		clientIP, _, err := net.SplitHostPort(req.RemoteAddr)
 		if err != nil {
+			log.Println(err)
 			http.Error(w, err.Error(), 400)
 			return
 		}
-		body, err := ioutil.ReadAll(req.Body)
+		body, err := ioutil.ReadAll(http.MaxBytesReader(w, req.Body, MaxRequestLength))
 		req.Body.Close()
 		if err != nil {
+			log.Println(err)
 			http.Error(w, err.Error(), 400)
 			return
 		}
 		// Try to inject our clientIP into the `user.ip_address` chunk
 		body, err = jsonparser.Set(body, []byte(`"`+clientIP+`"`), "user", "ip_address")
 		if err != nil {
+			log.Println(err)
 			http.Error(w, "invalid JSON body", 400)
 			return
 		}
 		// Rewrite our request for the proxy
+		req.Host = u.Host
 		req.URL.Scheme = u.Scheme
 		req.URL.Host = u.Host
+		req.URL.Path = u.Path + req.URL.Path
 		req.Header.Set("Host", u.Host)
 		req.ContentLength = int64(len(body))
 		req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-		// Sling out mutated request upstream
+		// Sling our mutated request upstream
 		proxy.ServeHTTP(w, req)
 	}
 }
 
-func main() {
+func init() {
 	flag.Parse()
-	server := &http.Server{
-		Addr:           ":8080",
-		Handler:        newHandler(*upstreamFlag),
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
+}
+
+func main() {
+	if *versionFlag {
+		fmt.Fprintf(os.Stderr, "sentry-proxy version: %s (%s on %s/%s; %s)\n", Version, runtime.Version(), runtime.GOOS, runtime.GOARCH, runtime.Compiler)
+		os.Exit(0)
 	}
-	log.Println(*upstreamFlag)
+
+	handler := logger.DefaultHandler(
+		newHandler(*upstreamFlag),
+	)
+
+	server := &http.Server{
+		Addr:           *listenFlag,
+		Handler:        handler,
+		ReadTimeout:    *readTimeoutFlag,
+		WriteTimeout:   *writeTimeoutFlag,
+		MaxHeaderBytes: 10 * 1024,
+	}
+
+	fmt.Println(`                _
+               | |
+ ___  ___ _ __ | |_ _ __ _   _ ______ _ __  _ __ _____  ___   _
+/ __|/ _ \ '_ \| __| '__| | | |______| '_ \| '__/ _ \ \/ / | | |
+\__ \  __/ | | | |_| |  | |_| |      | |_) | | | (_) >  <| |_| |
+|___/\___|_| |_|\__|_|   \__, |      | .__/|_|  \___/_/\_\\__, |
+                          __/ |      | |                   __/ |
+                         |___/       |_|                  |___/
+`)
+	fmt.Println("- listen: ", *listenFlag)
+	fmt.Println("- upstream: ", *upstreamFlag)
+	fmt.Println("- read-timeout: ", *readTimeoutFlag)
+	fmt.Println("- write-timeout: ", *writeTimeoutFlag)
+	fmt.Println("\n* Ready to serve.")
 	log.Fatal(server.ListenAndServe())
 }
